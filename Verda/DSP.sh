c8662beb33e6b91e8246e472df63ad0b59e5e384
BjornWest <bpf.westerlund@gmail.com>
2025-12-02 14:22:52 +0100

    remove pip

--- Verda/DSP.sh
+++ Verda/DSP.sh
@@ -3,8 +3,6 @@ apt install -y python3.12-dev
 apt install python3.12-venv
 python3 -m venv venv
 source venv/bin/activate
-curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-python3 /tmp/get-pip.py 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 python3 -m venv vllm_env
